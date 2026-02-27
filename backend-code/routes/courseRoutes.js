fba98d738064367a4318af79014d38dc74d73507
nishthaq3 <nishthatripathi05@gmail.com>
2026-02-27 14:34:38 +0530

    Course routes added

---
+++ backend-code/routes/courseRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createCourse,
+  getCourses,
+  deleteCourse
+} from "../controllers/courseController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", protect, createCourse);
+router.get("/", protect, getCourses);
+router.delete("/:id", protect, deleteCourse);
+
+export default router;
